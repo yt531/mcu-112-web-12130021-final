eeb61184088fe65fcb8a0afa68ac5a2e96516ce2
yt531 <yanting0531@gmail.com>
2024-01-12 16:15:46 +0800

    feat(body): 首頁內容元件實作新增

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from '../header/header.component';
+import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BodyComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
