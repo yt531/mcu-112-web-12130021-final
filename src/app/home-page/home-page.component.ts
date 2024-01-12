ef4ab74883b2a0a94bcae4419b1cbe6081b7c228
yt531 <yanting0531@gmail.com>
2024-01-12 15:28:14 +0800

    feat(header): 標題元件實作新增

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [HeaderComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
