0e6d45414d06243223eeeb81c7b2981abd239e94
yt531 <yanting0531@gmail.com>
2024-01-15 15:36:16 +0800

    feat(article-form): 新文章表單頁面切換實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { ArticleFormPageComponent } from './article-form-page/article-form-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomePageComponent },
   { path: 'sign-in', component: SignInPageComponent },
   { path: 'sign-up', component: SignUpPageComponent },
+  { path: 'article/form', component: ArticleFormPageComponent },
 ];
---
+++ src/app/article-form-page/article-form-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-article-form-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './article-form-page.component.html',
+  styleUrl: './article-form-page.component.css'
+})
+export class ArticleFormPageComponent {
+
+}
