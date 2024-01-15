9db2f6f89b6aeca8f42fd9f2c565f7c097f3c887
yt531 <yanting0531@gmail.com>
2024-01-15 21:15:59 +0800

    feat(model): 文章資料類別定義新增

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -2,15 +2,28 @@ import { FavoriteButtonComponent } from './../favorite-button/favorite-button.co
 import { Component, HostBinding } from '@angular/core';
 
 import { TagListComponent } from '../tag-list/tag-list.component';
+import { DatePipe } from '@angular/common';
+import { Article } from '../model/article';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [FavoriteButtonComponent, TagListComponent],
+  imports: [DatePipe, FavoriteButtonComponent, TagListComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
 export class ArticlePreviewComponent {
   @HostBinding('class') class = 'article-preview';
   tags = ['enim', 'repellat', 'est', 'eos'];
+  article = new Article({
+    id: 1,
+    title:
+      'Ill quantify the redundant TCP bus, that should hard drive the ADP bandwidth!',
+    content:
+      'Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae corporis corporis possimus.',
+    favoriteCount: 30,
+    author: 'Jeff',
+    createDate: new Date(2024, 6, 4),
+    tags: ['enim', 'repellat', 'est', 'eos'],
+  });
 }
---
+++ src/app/model/article.ts
@@ -0,0 +1,19 @@
+import { FavoriteButtonComponent } from './../favorite-button/favorite-button.component';
+export class Article {
+  constructor(initDate?: Partial<Article>) {
+    Object.assign(this, initDate);
+  }
+  id!: number;
+
+  title!: string;
+
+  content!: string;
+
+  favoriteCount!: number;
+
+  author!: string;
+
+  createDate!: Date;
+
+  tags!: string[];
+}
