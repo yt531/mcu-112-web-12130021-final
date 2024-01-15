185aac543dcb9abe6d9500ae621f2a959dc357d5
yt531 <yanting0531@gmail.com>
2024-01-15 21:48:59 +0800

    feat(article): 文章元件實作新增

--- src/app/article-preview/article-preview.component.ts
+++ src/app/article-preview/article-preview.component.ts
@@ -1,14 +1,12 @@
-import { FavoriteButtonComponent } from './../favorite-button/favorite-button.component';
 import { Component, HostBinding } from '@angular/core';
 
-import { TagListComponent } from '../tag-list/tag-list.component';
-import { DatePipe } from '@angular/common';
 import { Article } from '../model/article';
+import { ArticleComponent } from '../article/article.component';
 
 @Component({
   selector: 'app-article-preview',
   standalone: true,
-  imports: [DatePipe, FavoriteButtonComponent, TagListComponent],
+  imports: [ArticleComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.css',
 })
@@ -19,8 +17,11 @@ export class ArticlePreviewComponent {
     id: 1,
     title:
       'Ill quantify the redundant TCP bus, that should hard drive the ADP bandwidth!',
-    content:
-      'Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae corporis corporis possimus.',
+    content: `
+      Aut facilis qui. Cupiditate sit ratione eum sunt rerum impedit. Qui suscipit
+      debitis et et voluptates voluptatem voluptatibus. Quas voluptatum quae
+      corporis corporis possimus.
+      `,
     favoriteCount: 30,
     author: 'Jeff',
     createDate: new Date(2024, 6, 4),
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,17 @@
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
+import { TagListComponent } from '../tag-list/tag-list.component';
+import { Article } from '../model/article';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [DatePipe, FavoriteButtonComponent, TagListComponent],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css',
+})
+export class ArticleComponent {
+  @Input({ required: true })
+  article!: Article;
+}
